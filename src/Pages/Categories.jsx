32bb8cacaf1d8b0fe7469a50d9e37e2028af338a
N-meapp <developersnme@gmail.com>
2025-04-11 11:02:56 +0530

    merging

---
+++ src/Pages/Categories.jsx
@@ -0,0 +1,35 @@
+
+import LoadMoreButton from "../Components/Buttons/LoadMoreButton";
+import CategoryList from "../Components/CategoryList/CategoryList";
+import SecondarySearch from "../Components/Inputs/SecondarySearch";
+import { womensCategory } from "../constants/category";
+import { golds } from "../constants/products";
+import CategoryAboutSection from "../Layout/CategoryPage/CategoryAboutSection";
+import CategoryHeader from "../Layout/CategoryPage/CategoryHeader";
+import CategoryProductsListing from "../Layout/CategoryPage/CategoryProductsListing";
+import RelatedProducts from "../Layout/CategoryPage/RelatedProducts";
+
+
+export default function Categories() {
+    return (
+        <>
+
+            <CategoryHeader />
+            <SecondarySearch />
+
+<div className="mt-[40px]">
+                    <CategoryList />
+
+</div>
+
+            <CategoryProductsListing list={golds} />
+            <LoadMoreButton />
+            <CategoryAboutSection details={womensCategory} />
+            <RelatedProducts />
+            
+
+        </>
+    )
+}
+
+
